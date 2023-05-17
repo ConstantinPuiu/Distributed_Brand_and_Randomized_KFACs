f590a0f06c48d6baf2bcd2abaa3e4821767b6d24
ConstantinPuiu <constantin.puiu@maths.ox.ac.uk>
2023-05-17 10:56:08 +0100

    better work alloc for RKFAC to catch bugs

--- example_shell_files/trun_2_GPU_R_KFAC_lean_Cifar10.sh
+++ example_shell_files/trun_2_GPU_R_KFAC_lean_Cifar10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --time=00:59:59
-#SBATCH --job-name=2G_RK_1he
+#SBATCH --time=00:10:00
+#SBATCH --job-name=2G_RK
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=4
@@ -9,7 +9,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=single:1
 #SBATCH --mem-per-cpu=15G
-#SBATCH --partition=short
+#SBATCH --partition=devel
 
 module purge
 module load Anaconda3/2020.11
@@ -22,5 +22,5 @@ source activate /data/math-opt-ml/chri5570/myenv
 
 #mpiexec python ./attempt_2_GPUs_naive_KFAC.py
 #NCCL_BLOCKING_WAIT=1
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_R_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 10 --work_alloc_propto_RSVD_cost 1
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_R_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 10 --work_alloc_propto_RSVD_cost 1 --adaptable_rsvd_rank 1 --rank_adaptation_TInv_multiplier 1
 
