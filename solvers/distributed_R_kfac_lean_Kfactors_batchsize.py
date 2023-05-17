3fc1ef5217066e23eabf9a1c13bcb4ad8b47b0ad
Constantin Octavian Puiu <octavianpuiu_95@yahoo.com>
2023-05-17 11:52:09 +0100

    RKFAC adaptive rank bugfix: reinitialized the Q and d tensors correctly

--- solvers/distributed_R_kfac_lean_Kfactors_batchsize.py
+++ solvers/distributed_R_kfac_lean_Kfactors_batchsize.py
@@ -206,6 +206,10 @@ class R_KFACOptimizer(optim.Optimizer):
                     self.d_a[module] = 0 * aa[0,:actual_rank]; self.Q_a[module] = 0 * aa[:,:actual_rank] # Now we'll have Q_a's as skinnytall because
                     # we are using RSVD representation(lowrank) and thus we need to initialize our zeros accordngly
                     self.nkfu_dict_a[module] = 1
+                elif (self.steps - self.TCov) % (self.TInv * self.rank_adaptation_TInv_multiplier):
+                    # This is the first time we enter the HOOKS at TCov multiple AFTER a new TARGET-RANK reevaluation
+                    actual_rank = min(aa.shape[0], self.current_rsvd_ranks_a[module])
+                    self.d_a[module] = 0 * aa[0,:actual_rank]; self.Q_a[module] = 0 * aa[:,:actual_rank]
                 else:
                     self.nkfu_dict_a[module] += 1
                     
@@ -249,6 +253,10 @@ class R_KFACOptimizer(optim.Optimizer):
                     self.d_g[module] = 0 * gg[0,:actual_rank]; self.Q_g[module] = 0 * gg[:,:actual_rank] # Now we'll have Q_g's as skinnytall because
                     # we are using RSVD representation(lowrank) and thus we need to initialize our zeros accordngly
                     self.nkfu_dict_g[module] = 1
+                elif (self.steps - self.TCov) % (self.TInv * self.rank_adaptation_TInv_multiplier):
+                    # This is the first time we enter the HOOKS at TCov multiple AFTER a new TARGET-RANK reevaluation
+                    actual_rank = min(gg.shape[0], self.current_rsvd_ranks_g[module])
+                    self.d_g[module] = 0 * gg[0,:actual_rank]; self.Q_g[module] = 0 * gg[:,:actual_rank]
                 else:
                     self.nkfu_dict_g[module] += 1
                     
