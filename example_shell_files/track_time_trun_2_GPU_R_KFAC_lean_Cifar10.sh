11b58094c37d0528c2373fca4f0b65978e548c48
ConstantinPuiu <constantin.puiu@maths.ox.ac.uk>
2023-05-19 14:48:09 +0100

    tiny change to time-track .sh file

--- example_shell_files/track_time_trun_2_GPU_R_KFAC_lean_Cifar10.sh
+++ example_shell_files/track_time_trun_2_GPU_R_KFAC_lean_Cifar10.sh
@@ -22,5 +22,5 @@ source activate /data/math-opt-ml/chri5570/myenv
 
 #mpiexec python ./attempt_2_GPUs_naive_KFAC.py
 #NCCL_BLOCKING_WAIT=1
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/track_time_n_GPUs_dist_R_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 10 --work_alloc_propto_RSVD_cost 0 --adaptable_rsvd_rank 1 --rank_adaptation_TInv_multiplier 1
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/track_time_n_GPUs_dist_R_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 10 --work_alloc_propto_RSVD_cost 1 --adaptable_rsvd_rank 1 --rank_adaptation_TInv_multiplier 1
 
