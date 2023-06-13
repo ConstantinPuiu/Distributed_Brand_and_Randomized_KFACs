3021fa95a63dfa19e14806d12d6f58b71a9b2db8
ConstantinPuiu <constantin.puiu@maths.ox.ac.uk>
2023-06-13 17:33:04 +0100

    .sh files: minor changes to reflect functionality implemeted in previous commit

--- example_shell_files/trun_2_GPU_B_pure_KFAC_lean_Cifar10.sh
+++ example_shell_files/trun_2_GPU_B_pure_KFAC_lean_Cifar10.sh
@@ -23,4 +23,4 @@ source activate /data/math-opt-ml/chri5570/myenv
 #mpiexec python ./attempt_4_GPUs_naive_KFAC.py
 #NCCL_BLOCKING_WAIT=1
 #OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_B_R_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epochs 10 --brand_period 5000000000000000 --brand_update_multiplier_to_TCov 5
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_B_pure_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epochs 10 --brand_update_multiplier_to_TCov 5 --work_alloc_propto_RSVD_and_B_cost 1
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_B_pure_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epochs 10 --brand_update_multiplier_to_TCov 5 --work_alloc_propto_RSVD_and_B_cost 1 --truncate_before_inversion 1
