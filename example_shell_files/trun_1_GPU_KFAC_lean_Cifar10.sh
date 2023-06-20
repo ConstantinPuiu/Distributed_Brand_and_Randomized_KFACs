6b590df2d7de76b4e5595e38e0dcaf9a2014bc06
Constantin Octavian Puiu <octavianpuiu_95@yahoo.com>
2023-06-20 18:27:54 +0100

    KFAC .sh changed according to prev modif and be runable on devel

--- example_shell_files/trun_1_GPU_KFAC_lean_Cifar10.sh
+++ example_shell_files/trun_1_GPU_KFAC_lean_Cifar10.sh
@@ -9,7 +9,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=single:1
 #SBATCH --mem-per-cpu=15G
-#SBATCH --partition=short
+#SBATCH --partition=devel
 
 module purge
 module load Anaconda3/2020.11
@@ -21,7 +21,7 @@ source activate /data/math-opt-ml/chri5570/myenv
 #optDLvenv
 
 #mpiexec python ./attempt_4_GPUs_naive_KFAC.py
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=1 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 1 --n_epoch 5 \
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=1 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 1 --n_epoch 3 \
 --work_alloc_propto_EVD_cost 1 \
 --TInv_schedule_flag 0 --TCov_schedule_flag 0 --KFAC_damping_schedule_flag 0
 
--- example_shell_files/trun_2_GPU_KFAC_lean_Cifar10.sh
+++ example_shell_files/trun_2_GPU_KFAC_lean_Cifar10.sh
@@ -22,7 +22,7 @@ source activate /data/math-opt-ml/chri5570/myenv
 
 #mpiexec python ./attempt_2_GPUs_naive_KFAC.py
 #NCCL_BLOCKING_WAIT=1
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 10 \
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/CIFAR10/n_GPUs_dist_KFAC_torchrun_lean_KFACTORS_CIFAR_10.py --world_size 2 --n_epoch 6 \
 --work_alloc_propto_EVD_cost 1 \
 --TInv_schedule_flag 0 --TCov_schedule_flag 0 --KFAC_damping_schedule_flag 0
 
