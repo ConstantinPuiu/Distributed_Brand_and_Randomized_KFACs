1176955b116c5d564236fb956a85873461470652
Constantin Octavian Puiu <octavianpuiu_95@yahoo.com>
2023-08-23 18:30:12 +0100

    .sh files for previous commit

--- example_shell_files/CIFAR10_sh_files/get_eigenspectrums_2_sf_2_GPU_KFAC_lean_Cifar10.sh
+++ example_shell_files/CIFAR10_sh_files/get_eigenspectrums_2_sf_2_GPU_KFAC_lean_Cifar10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=08:45:00
-#SBATCH --job-name=4_espectrums
+#SBATCH --time=04:59:59
+#SBATCH --job-name=scf2_2G
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4 --constraint='gpu_sku:A100'
+#SBATCH --gres=gpu:2 --constraint='gpu_sku:V100'
 #SBATCH --cpus-per-task=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=2
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=single:1
 #SBATCH --mem-per-cpu=15G
@@ -22,7 +22,7 @@ source activate /data/math-opt-ml/chri5570/myenv
 
 #mpiexec python ./attempt_2_GPUs_naive_KFAC.py
 #NCCL_BLOCKING_WAIT=1
-OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=4 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/main_measuring_Kfactors_spectrums.py --world_size 4 --n_epoch 20 --batch_size 128 \
+OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=2 /home/chri5570/Distributed_Brand_and_Randomized_KFACs/main_files/main_measuring_Kfactors_spectrums.py --world_size 2 --n_epoch 30 --batch_size 128 \
 --stop_at_test_acc 0 --stopping_test_acc 92 \
 --kfac_clip 0.07 --stat_decay 0.95 --momentum 0.0 --WD 0.0007 \
 --lr_schedule_type 'staircase' --base_lr 0.1 --lr_decay_rate 3 --lr_decay_period 6 --auto_scale_forGPUs_and_BS 1 \
@@ -35,4 +35,5 @@ OMP_NUM_THREADS=8 torchrun --standalone --nnodes 1 --nproc_per_node=4 /home/chri
 --TInv_period 100 --TCov_period 20 \
 --work_alloc_propto_EVD_cost 1 \
 --TInv_schedule_flag 0 --TCov_schedule_flag 0 --KFAC_damping_schedule_flag 0 \
---Kfactor_spectrum_savepath '/data/math-opt-ml/saved_eigenspectrums/' --Network_scalefactor 8
+--Kfactor_spectrum_savepath '/data/math-opt-ml/saved_eigenspectrums/' --Network_scalefactor 2
+
